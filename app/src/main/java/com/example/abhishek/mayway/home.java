c7480d4274fd9e7b4e62f3a7f3cb5ae19e9f4713
Abhishek Yadav <abhishekcms611@gmail.com>
2017-07-19 14:21:00 +0530

    framework is done

---
+++ app/src/main/java/com/example/abhishek/mayway/home.java
@@ -0,0 +1,52 @@
+package com.example.abhishek.mayway;
+
+import android.content.Intent;
+import android.content.pm.ActivityInfo;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class home extends AppCompatActivity {
+    @Override
+    public void onWindowFocusChanged(boolean hasFocas){
+        super.onWindowFocusChanged(hasFocas);
+        View decorView=getWindow().getDecorView();
+        if(hasFocas){
+            decorView.setSystemUiVisibility(View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                    | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY
+                    | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+                    |View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                    | View.SYSTEM_UI_FLAG_FULLSCREEN
+                    | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION);
+        }
+    }
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setRequestedOrientation (ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE);
+        setContentView(R.layout.activity_home);
+        Button b1 = (Button)findViewById(R.id.button);
+
+
+        b1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                call();
+            }
+        });
+
+
+    }
+
+    public void call()
+    {
+        Intent i = new Intent(this,login.class);
+        startActivity(i);
+
+    }
+}
+
---
+++ app/src/main/java/com/example/abhishek/mayway/play.java
@@ -0,0 +1,48 @@
+package com.example.abhishek.mayway;
+
+import android.os.Bundle;
+
+import android.graphics.Point;
+
+
+
+import android.view.Display;
+import android.view.MotionEvent;
+import android.view.View;
+
+import android.widget.Toast;
+
+import java.util.concurrent.TimeUnit;
+
+public class play extends functions  {
+    int k=0;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_play);
+        Display display = getWindowManager().getDefaultDisplay();
+        Point size = new Point();
+        display.getSize(size);
+        width = size.x;
+        height = size.y;
+        cx=width/7;
+        cy=height/7;
+        listenchk=1;
+        //listen();
+
+
+        initialise1();
+        draw();
+
+    }
+
+    public boolean onTouchEvent(MotionEvent event) {
+        float X = event.getX();
+        float Y = event.getY();
+        boolean  tell=ammend(X,Y);
+        ag=again(X,Y);
+        draw();
+        return true;
+    }
+}
